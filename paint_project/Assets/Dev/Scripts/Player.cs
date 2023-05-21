30e3df3ba54fea2b52943acf8eb404c430473bc8
martinrojasmet <martin.rojas@correo.unimet.edu.ve>
2023-05-21 11:18:40 -0400

    player color change 0.5 and physics

---
+++ paint_project/Assets/Dev/Scripts/Player.cs
@@ -0,0 +1,37 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    Color presetColor = new Color(207, 255, 0);
+    Color life2Color = new Color(255, 0, 255);
+    Color life1Color = new Color(0, 255, 255);
+    private int health = 3;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        var spriteRenderer = GetComponent<SpriteRenderer>();
+        spriteRenderer.color = presetColor;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void Hit() {
+        health--;
+        if (health == 2) {
+            var spriteRenderer = GetComponent<SpriteRenderer>();
+            spriteRenderer.color = life2Color;
+        } else if (health == 1) {
+            var spriteRenderer = GetComponent<SpriteRenderer>();
+            spriteRenderer.color = life1Color;
+        } else if (health <= 0) {
+            Destroy(gameObject);
+            }
+    }
+}
