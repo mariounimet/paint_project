37b07e51384aaef1ccc51a9467c30d4808b65034
Nicolas Briceño <nicolas.briceno@correo.unimet.edu.ve>
2023-05-21 18:58:34 -0400

    Spawner + ObjectPool working
    Ahora sí funciona con el objectPool el spawner, próximo commit es probar la funcionalidad cuando un enemigo muere

--- paint_project/Assets/Dev/Scripts/Pattern/Spawner.cs
+++ paint_project/Assets/Dev/Scripts/Pattern/Spawner.cs
@@ -1,20 +1,30 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using System.Linq;
 
 public class Spawner : MonoBehaviour
 {
     ObjectPooler objectPooler;
-    [SerializeField] private float spawnRate = 1f;
+    [SerializeField] private float spawnRate = 3f;
+    [SerializeField] private bool canSpawn = true;
 
     private void Start()
     {
         objectPooler = ObjectPooler.Instance;
+        StartCoroutine(Factory());
     }
 
-    void FixedUpdate () {
+    private IEnumerator Factory () {
         WaitForSeconds wait = new WaitForSeconds(spawnRate);
+        
+        while (canSpawn) {
+            yield return wait;
+            
+            int index = Random.Range(0, objectPooler.poolDictionary.Count);
+            string enemyToSpawn = objectPooler.poolDictionary.ElementAt(index).Key;
 
-        objectPooler.SpawnFromPool("Enemy", transform.position, Quaternion.identity);
+            objectPooler.SpawnFromPool(enemyToSpawn, transform.position, Quaternion.identity);
+        }
     }
 }
