938c1d3946390178c9b8bd372a5ee64034b879c0
lddg0502 <ldomingos@correo.unimet.edu.ve>
2023-05-22 11:05:00 -0400

    Bullet shooting

--- paint_project/Assets/Dev/Scripts/Player.cs
+++ paint_project/Assets/Dev/Scripts/Player.cs
@@ -7,7 +7,13 @@ public class Player : MonoBehaviour
     Color presetColor = new Color(207, 255, 0);
     Color life2Color = new Color(255, 0, 255);
     Color life1Color = new Color(0, 255, 255);
+
     private int health = 3;
+
+    [SerializeField] private GameObject bulletPrefab;
+    [SerializeField] private Transform firingPoint;
+    // [Range(0.1f, 1f)]
+    // [SerializeField] private float fireRate = 0.5f;
 
     // Start is called before the first frame update
     void Start()
@@ -19,9 +25,12 @@ public class Player : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        Shoot();
+    }
+    private void Shoot(){
+        Instantiate(bulletPrefab, firingPoint.position, firingPoint.rotation);
         
     }
-
     public void HitBullet() {
         health--;
         if (health == 2) {
