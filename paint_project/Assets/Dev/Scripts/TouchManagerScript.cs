2083702f56789b8f2a1f5d24b4a9c569acd460f2
isaacBakugan <97451474+OlivaresIsaac@users.noreply.github.com>
2023-05-21 22:41:10 -0400

    add momentum to player movement

--- paint_project/Assets/Dev/Scripts/TouchManagerScript.cs
+++ paint_project/Assets/Dev/Scripts/TouchManagerScript.cs
@@ -21,7 +21,12 @@ public class TouchManagerScript : MonoBehaviour
     private float moveForward;
     public float RotationSpeed;
     private float rotZ;
+    private float currentAngleZ;
     public float drag;
+    private float velX;
+    private float velY;
+  
+    public Vector2 maxVel;
 
     private void Awake() {
       playerInput = GetComponent<PlayerInput>();
@@ -32,10 +37,8 @@ public class TouchManagerScript : MonoBehaviour
       
       middleOfScreen = Camera.main.transform.position.x;
       Debug.Log(middleOfScreen);
-    }
 
-    public void setMiddleOfScreen(float newX){
-      this.middleOfScreen = newX;
+      //this.currentAngleZ = 0;
     }
 
     // private void OnEnable() {
@@ -57,25 +60,34 @@ public class TouchManagerScript : MonoBehaviour
     // }
 
     private void Accelerate() {
-        Vector3 playerPos = player.transform.position;
+      
         double degreeInclination = (90 + player.transform.rotation.eulerAngles.z) * Math.PI / 180.0f;
         Debug.Log(degreeInclination);
 
-        playerPos.x += (float) (Time.deltaTime * Speed * Math.Cos(degreeInclination));
-        playerPos.y += (float) (Time.deltaTime * Speed * Math.Sin(degreeInclination));
-        player.transform.position = playerPos; 
+        this.velX += (float) (Time.deltaTime * Speed * Math.Cos(degreeInclination));
+        this.velY += (float) (Time.deltaTime * Speed * Math.Sin(degreeInclination));
+
+
+
+        // playerPos.x += (float) (Time.deltaTime * Speed * Math.Cos(degreeInclination));
+        // playerPos.y += (float) (Time.deltaTime * Speed * Math.Sin(degreeInclination));
+        // player.transform.position = playerPos; 
+       
     }
 
     private void Update() {
+      UpdateMovement();
       if (touchPressAction2.ReadValue<float>() > 0.0f) {
         Accelerate();
       } else {
         if(touchPressAction.ReadValue<float>() > 0.0f) {
           Vector3 tapPosition = Camera.main.ScreenToWorldPoint(touchPositionAction.ReadValue<Vector2>());
         if (tapPosition.x > middleOfScreen) {
-          rotZ -= RotationSpeed * Time.deltaTime;
+          this.rotZ -= RotationSpeed * Time.deltaTime;
+          
         } else {
-          rotZ += RotationSpeed * Time.deltaTime;
+          this.rotZ += RotationSpeed * Time.deltaTime;
+       
         }
 
           // Debug.Log(tapPosition);
@@ -84,6 +96,58 @@ public class TouchManagerScript : MonoBehaviour
       }
       
       
+    }
+
+    private void UpdateMovement(){
+      Vector3 playerPos = player.transform.position;
+     
+      if (this.velX > 0) {
+          this.velX -= drag;
+          this.velX = (this.velX > this.maxVel.x) ? this.maxVel.x : this.velX;
+      } else {
+          this.velX += drag;
+         this.velX = (this.velX < this.maxVel.x*-1) ? this.maxVel.x*-1 : this.velX;
+      }
+
+      if (this.velY > 0) {
+          this.velY -= drag;
+          this.velY = (this.velY > this.maxVel.y) ? this.maxVel.y : this.velY;
+        
+      } else {
+          this.velY += drag;
+          this.velY = (this.velY < this.maxVel.y*-1) ? this.maxVel.y*-1 : this.velY;
+
+      }
+
+      if (Mathf.Abs(this.velX) > 0.01f)  {
+        playerPos.x += this.velX;
+      }
+      if (Mathf.Abs(this.velY) > 0.01f) {
+          playerPos.y += this.velY;
+      }
+      
+      player.transform.position = playerPos; 
+
+
+      // if (this.rotZ > 0) {
+      //   this.rotZ -= drag;
+      //   this.rotZ = (this.rotZ > this.maxVel.z) ? this.maxVel.z : this.rotZ;
+      // } else {
+      //   this.rotZ += drag;
+      //   this.rotZ = (this.rotZ < this.maxVel.z*-1) ? this.maxVel.z*-1 : this.rotZ;
+      // }
+
+      // this.currentAngleZ += this.rotZ;
+
+      // //if (Math.Abs(rotZ) > 0.5) {
+      //   player.transform.rotation = Quaternion.Euler(0, 0, this.currentAngleZ);
+      // //}
+      
+    }
+
+
+    public void setMiddleOfScreen(float newX){
+      this.middleOfScreen = newX;
     }
     
 }
