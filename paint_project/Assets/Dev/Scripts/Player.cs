f7684a29ac89815ca46630f4b207a4fd95bcda96
isaacBakugan <97451474+OlivaresIsaac@users.noreply.github.com>
2023-05-21 23:32:40 -0400

    complete spawner merge

--- paint_project/Assets/Dev/Scripts/Player.cs
+++ paint_project/Assets/Dev/Scripts/Player.cs
@@ -9,21 +9,21 @@ public class Player : MonoBehaviour
     Color life1Color = new Color(0, 255, 255);
     private int health = 3;
 
-        [SerializeField] private float speed = 5f;
+        // [SerializeField] private float speed = 5f;
 
     //Gun variables
-    [SerializeField] private GameObject bulletPrefab;
-    [SerializeField] private Transform firingPoint;
-    [Range(0.1f, 2f)]
-    [SerializeField] private float fireRate = 0.5f;
+    // [SerializeField] private GameObject bulletPrefab;
+    // [SerializeField] private Transform firingPoint;
+    // [Range(0.1f, 2f)]
+    // [SerializeField] private float fireRate = 0.5f;
 
-    private Rigidbody2D rb;
-    private float mx;
-    private float my;
+    // private Rigidbody2D rb;
+    // private float mx;
+    // private float my;
 
-    private float fireTimer;
+    // private float fireTimer;
 
-    private Vector2 mousePos;
+    // private Vector2 mousePos;
 
   
 
@@ -33,7 +33,7 @@ public class Player : MonoBehaviour
         var spriteRenderer = GetComponent<SpriteRenderer>();
         spriteRenderer.color = presetColor;
 
-         rb = GetComponent<Rigidbody2D>();
+        // rb = GetComponent<Rigidbody2D>();
     }
 
     // Update is called once per frame
@@ -61,28 +61,28 @@ public class Player : MonoBehaviour
 
 
     private void Update() {
-        mx = Input.GetAxisRaw("Horizontal");
-        my = Input.GetAxisRaw("Vertical");
-        mousePos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+        // mx = Input.GetAxisRaw("Horizontal");
+        // my = Input.GetAxisRaw("Vertical");
+        // mousePos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
 
-        float angle = Mathf.Atan2(mousePos.y - transform.position.y, mousePos.x -
-        transform.position.x) * Mathf.Rad2Deg - 90f;
+        // float angle = Mathf.Atan2(mousePos.y - transform.position.y, mousePos.x -
+        // transform.position.x) * Mathf.Rad2Deg - 90f;
 
-        transform.localRotation = Quaternion.Euler(0, 0, angle);
+        // transform.localRotation = Quaternion.Euler(0, 0, angle);
 
-        if(Input.GetMouseButtonDown(0) && fireTimer <= 0f) {
-            Shoot();
-            fireTimer = fireRate;
-        } else {
-            fireTimer -= Time.deltaTime;
-        }
+        // if(Input.GetMouseButtonDown(0) && fireTimer <= 0f) {
+        //     Shoot();
+        //     fireTimer = fireRate;
+        // } else {
+        //     fireTimer -= Time.deltaTime;
+        // }
     }
 
-    private void FixedUpdate() {
-        rb.velocity = new Vector2(mx, my).normalized * speed;
-    }
+    // private void FixedUpdate() {
+    //     rb.velocity = new Vector2(mx, my).normalized * speed;
+    // }
 
-    private void Shoot() {
-        Instantiate(bulletPrefab, firingPoint.position, firingPoint.rotation);
-    }
+    // private void Shoot() {
+    //     Instantiate(bulletPrefab, firingPoint.position, firingPoint.rotation);
+    // }
 }
