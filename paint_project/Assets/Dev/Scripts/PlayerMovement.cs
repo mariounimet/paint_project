59b550fe17032b98b1af3775f8893a49b70c3698
Emilio Ferrer <39682384+FerrerChakibaCode@users.noreply.github.com>
2023-05-30 23:28:55 -0400

    Primer approach al movimiento + Rotacion en el mismo joystick

---
+++ paint_project/Assets/Dev/Scripts/PlayerMovement.cs
@@ -0,0 +1,48 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+
+public class PlayerMovement : MonoBehaviour
+{
+
+  [SerializeField] private Rigidbody2D player;
+  [SerializeField] public float speed;
+  [SerializeField] private FixedJoystick _joystick;
+  private double angle;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+
+        player.velocity = new Vector3(_joystick.Horizontal * speed, _joystick.Vertical * speed, 0);
+        Debug.Log("horizontal: " + _joystick.Horizontal);
+        Debug.Log("vertical: " + _joystick.Vertical);
+
+        if (_joystick.Horizontal > 0 && _joystick.Vertical > 0) {
+          angle = Math.Atan(_joystick.Vertical / _joystick.Horizontal) * 180/Math.PI + 90;
+        } 
+        
+          else if (_joystick.Horizontal < 0 && _joystick.Vertical > 0) {
+          angle = (Math.Atan(_joystick.Horizontal / _joystick.Vertical))  * 180/Math.PI + 180;
+        } 
+        
+          else if (_joystick.Horizontal < 0 && _joystick.Vertical < 0){
+          angle = (Math.Atan(_joystick.Vertical / _joystick.Horizontal))  * 180/Math.PI + 270;
+        } 
+        
+          else {
+          angle = (Math.Atan(_joystick.Horizontal / _joystick.Vertical))  * 180/Math.PI;
+        }
+
+        player.rotation = (float) angle + 180;
+        // Debug.Log("angulo: " + angle * 180 / Math.PI);
+        Debug.Log("angulo: " + angle);
+        // player.transform.rotation = Quaternion.Euler(0, 0, _joystick.);
+    }
+}
