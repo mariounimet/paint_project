c7c144be8fb52b52240065c5e5b535d5164843e6
lddg0502 <ldomingos@correo.unimet.edu.ve>
2023-06-11 18:08:36 -0400

    Ahora lo ves, ahora no lo ves

--- paint_project/Assets/Dev/Scripts/LevelSelector.cs
+++ paint_project/Assets/Dev/Scripts/LevelSelector.cs
@@ -9,10 +9,17 @@ public class LevelSelector : MonoBehaviour
     public Vector3 LevelMenuCords;
     public PaintManagerScript paintManager;
     private Camera mainCamera;
+    public GameObject joystick;
+    public GameObject progressBar;
+    public GameObject text;
     // Start is called before the first frame update
     void Start()
     {
+        joystick.gameObject.SetActive(false);
+        progressBar.gameObject.SetActive(false);
+        text.gameObject.SetActive(false);
         mainCamera = Camera.main;
+
         
     }
     // Update is called once per frame
@@ -24,6 +31,9 @@ public class LevelSelector : MonoBehaviour
     public void lvlSelector(){
         print("Soy un nivel ");
         paintManager.curreentImage = imgIndex;
+        joystick.gameObject.SetActive(true);
+        progressBar.gameObject.SetActive(true);
+        text.gameObject.SetActive(true);
         this.mainCamera.transform.position = new Vector3(this.LevelMenuCords.x,this.LevelMenuCords.y,this.LevelMenuCords.z);
         
     }
