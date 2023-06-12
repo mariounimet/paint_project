616845965a4ee7c51672b7d43eed915db6157cfd
martinrojasa <97409637+martinrojasmet@users.noreply.github.com>
2023-06-12 17:39:58 -0400

    tutorial popups

--- paint_project/Assets/Dev/Scripts/TutorialManager.cs
+++ paint_project/Assets/Dev/Scripts/TutorialManager.cs
@@ -1,59 +1,70 @@
-// using System.Collections;
-// using System.Collections.Generic;
-// using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
 
-// public class TutorialManager : MonoBehaviour
-// {
-//     public GameObject[] popUps;
-//     public int popUpIndex = 0;
+public class TutorialManager : MonoBehaviour
+{
+    public GameObject[] popUps;
+    public int popUpIndex = 0;
+    private bool isScreenTouched = false;
 
-//     // Start is called before the first frame update
-//     void Start()
-//     {
-        
-//     }
+    // Start is called before the first frame update
+    void Start()
+    {
+        popUps[0].SetActive(true);
+        popUps[1].SetActive(false);
+        popUps[2].SetActive(false);
+    }
 
-//     // Update is called once per frame
-//     void Update()
-//     {
-//         UpdatePopUpIndex();
-//         UpdatePopUp();
-//     }
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.touchCount > 0)
+        {
+            Touch touch = Input.GetTouch(0);
+            if (touch.phase == TouchPhase.Began)
+            {
+                isScreenTouched = true;
+                UpdatePopUpIndex();
+            }
+            else if (touch.phase == TouchPhase.Ended || touch.phase == TouchPhase.Canceled)
+            {
+                isScreenTouched = false;
+            }
+        }
+        else
+        {
+            isScreenTouched = false;
+        }
+    }
 
-//     void UpdatePopUp()
-//     {
-//         for (int i = 0; i < popUps.Length; i++)
-//         {
-//             if (i == popUpIndex)
-//             {
-//                 popUps[i].SetActive(true);
-//             }
-//             else
-//             {
-//                 popUps[i].SetActive(false);
-//             }
-//         }
-//     }
+    void UpdatePopUp()
+    {
+        for (int i = 0; i < popUps.Length; i++)
+        {
+            if (i == popUpIndex)
+            {
+                popUps[i].SetActive(true);
+            }
+            else
+            {
+                popUps[i].SetActive(false);
+            }
+        }
+    }
 
-//     void UpdatePopUpIndex()
-//     {
-//         if (popUpIndex == 0)
-//         {
-//             if (Input.GetKeyDown(KeyCode.LeftArrow) || Input.GetKeyDown(KeyCode.RightArrow))
-//             {
-//                 popUpIndex++;
-//             }
-//         } else if (popUpIndex == 1) {
-//             if (Input.GetKeyDown(KeyCode.UpArrow) || Input.GetKeyDown(KeyCode.DownArrow))
-//             {
-//                 popUpIndex++;
-//             }
-//         } else if (popUpIndex == 2) {
-//             if (Input.GetKeyDown(KeyCode.UpArrow) || Input.GetKeyDown(KeyCode.DownArrow))
-//             {
-//                 popUpIndex++;
-//                 //spawner
-//             }
-//         }
-//     }
-// }
+    void UpdatePopUpIndex()
+    {
+        if (popUpIndex == 0)
+        {
+                popUpIndex++;
+                UpdatePopUp();
+        } else if (popUpIndex == 1) {
+                popUpIndex++;
+                UpdatePopUp();
+        } else if (popUpIndex == 2) {
+                popUpIndex++;
+                UpdatePopUp();
+        }
+    }
+}
